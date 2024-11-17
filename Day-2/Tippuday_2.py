0254ddb958645912284a4ea98d5ac9efb81bd62b
stippusahib <stippusahib2028@cs.sjcetpalai.ac.in>
2024-11-18 00:35:29 +0530

    Update Tippuday_2.py

--- Day-2/Tippuday_2.py
+++ Day-2/Tippuday_2.py
@@ -1,3 +1,7 @@
+''' AUTHOR= S TIPPU SAHIB
+DESCRIPTION = Create a Python Program to Ask for Name and Age
+ Write a Python program that takes the user's name and age as input, then prints a greeting message.
+'''
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 print(f"Hello, {name}! You are {age} years old.")
