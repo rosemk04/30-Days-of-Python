c536140749829eb8cdb86a459781b5ed3eb78dad
stippusahib <stippusahib2028@cs.sjcetpalai.ac.in>
2024-11-18 00:41:56 +0530

    Create Tippu_day3.py

---
+++ Day-3/Tippu_day3.py
@@ -0,0 +1,10 @@
+''' AUTHOR : S TIPPU SAHIB
+DESCRIPTION: Check Whether a Person is a Minor or an Adult
+ Write a Python program that determines if a person is a minor or an adult based on their age.
+'''
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+  
