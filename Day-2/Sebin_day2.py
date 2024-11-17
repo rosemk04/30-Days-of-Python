202192a0e2a526c167bc101e2f64b81c68d9711d
SebinPrince <sebinpaikada1@gmail.com>
2024-11-17 20:39:33 +0530

    Create Sebin_day2.py

---
+++ Day-2/Sebin_day2.py
@@ -0,0 +1,7 @@
+'''AUTHOR=SEBIN PRINCE
+DESCRIPTION=Create a Python Program to Ask for Name and Age
+ Write a Python program that takes the user's name and age as input, then prints a greeting message.
+name=str(input("Enter your name:"))
+age=int(input("Enter your age:"))
+print("Hello",name,"!","you are",age,"years old")
+'''
