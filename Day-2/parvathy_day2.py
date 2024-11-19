3fc174a13cfaa2cd71f285d7520a98e539ee2da1
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-19 22:01:44 +0530

    Add files via upload

---
+++ Day-2/parvathy_day2.py
@@ -0,0 +1,5 @@
+'''Author:Paravthy J Nair
+DEscription:Create a Python Program to Ask for Name and Age'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print(f"Hello,{name}!,You are {age} years old")
