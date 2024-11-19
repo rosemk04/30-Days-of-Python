b9da779df9ff4625015c328f94b0bdfdd84662b4
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-11-19 10:04:32 -0800

    Add files via upload

---
+++ Day-4-part-B/Riya Ann Mathew .Day 4 part b.py
@@ -0,0 +1,6 @@
+''' Author :Riya ann Mathew
+Description:create a python program that calculates and prints the sum of numbers from 1 to 15  '''
+total=0
+for numbers in range(1,16):
+    total+=numbers
+    print(f"sum of numbers from 1 to 15 is :{total}")
