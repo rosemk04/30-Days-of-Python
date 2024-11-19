89722053cec169d0bd96dcbed2236581ebfaaa0d
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-11-19 09:21:12 -0800

    Add files via upload

---
+++ Day-2/Riya ann mathew day2.py
@@ -0,0 +1,5 @@
+'''Author:Riya Ann Mathew
+Description:create a python program to ask for name and age'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print(f"Hello,{name}!You are {age} years old.")
