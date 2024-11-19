0dc3cc67714af8f4b31c7ea4375e06f4ae23a3c7
SebinPrince <sebinpaikada1@gmail.com>
2024-11-19 23:09:18 +0530

    Update Sebin_day4.py

--- Sebin_day4.py
+++
@@ -1,9 +0,0 @@
-'''AUTHOR=SEBIN PRINCE
-DESCRIPTION= Write a Program to Check if a Number is Even or Odd Objective: Create a Python program that takes a number as input and determines if it is even or odd.
-while True:
-    a=int(input("Enter a number:"))
-    if a%2==0:
-        print(a,"is a even number")
-    else:
-        print(a,"is a odd number")
-'''
