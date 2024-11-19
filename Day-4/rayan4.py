09bf4f75d9c7aa1eefaf3e70d0abb9870a3ae5b1
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-19 23:17:40 +0530

    Add files via upload

---
+++ Day-4/rayan4.py
@@ -0,0 +1,17 @@
+'''
+Author:rayan joomy
+Description: Program to Swap the Values of Two Variables
+'''
+
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+print("Before swapping:")
+print(f"a = {a}, b = {b}")
+num=b
+
+b=a
+a=num
+
+print("\nAfter swapping:")
+print(f"a = {a}, b = {b}")
