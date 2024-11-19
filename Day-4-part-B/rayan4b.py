8ca739890dac9d570c0e11604ff467f2187fdd4f
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-19 23:34:11 +0530

    Add files via upload

---
+++ Day-4-part-B/rayan4b.py
@@ -0,0 +1,9 @@
+'''
+Author:RAYAN JOOMY
+Description: Python program that calculates and prints the sum of numbers from 1 to 15.
+'''
+sum=0
+for i in range(16):
+    sum+=i
+    
+print(f"the total sum of numbers from 1 to 15 is: {sum}")
