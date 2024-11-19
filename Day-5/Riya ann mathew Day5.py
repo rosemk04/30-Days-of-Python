6893009c418154bf7a532b792ac492c027898f19
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-11-19 10:21:39 -0800

    Add files via upload

---
+++ Day-5/Riya ann mathew Day5.py
@@ -0,0 +1,7 @@
+''' Author:Riya Ann Mathew
+Description:Write a program to check if a number is Even or odd objective:create a python program that takes a number as input and determines if it is even or odd'''
+a=int(input("Enter th number:"))
+if a %2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
