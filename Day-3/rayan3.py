489cc1198ffe7d219b8d7812e44be0a5135193e8
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-19 23:04:34 +0530

    Add files via upload

---
+++ Day-3/rayan3.py
@@ -0,0 +1,9 @@
+''' AUTHOR : RAYAN JOOMY
+DESCRIPTION: Check Whether a Person is a Minor or an Adult
+ Write a Python program that determines if a person is a minor or an adult based on their age.
+'''
+age=int(input("enter the age:"))
+if age>18 :
+    print("the person is an adult")
+else:
+    print("the person is an minor")
