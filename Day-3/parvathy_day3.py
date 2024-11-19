f0900aac0aabd7fb0bda88ddfec75aa83745d43c
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-19 22:07:58 +0530

    Add files via upload

---
+++ Day-3/parvathy_day3.py
@@ -0,0 +1,8 @@
+'''Author:Parvathy J Nair
+Description: Write a Python program that determines if a person is a minor or
+ an adult based on their age.'''
+age=int(input("Enter your age:"))
+if (age>18):
+    print("You are an adult")
+else:
+    print("You are a minor")
