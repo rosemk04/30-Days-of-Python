625ae11cade9106bbbfca4eabb671e5a7e976953
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-19 23:24:04 +0530

    Add files via upload

---
+++ Day-5/rayan5.py
@@ -0,0 +1,10 @@
+
+'''AUTHOR=RAYAN JOOMY
+DESCRIPTION= Write a Program to Check if a Number is Even or Odd Objective: 
+Create a Python program that takes a number as input and determines if it is even or odd
+'''
+a=int(input("enter the number:"))
+if a%2==0 :
+    print("the number is even")
+else:
+    print("the number is odd")
