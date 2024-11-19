5a7a641ab14927f348a2e83b9c27b5c2447073a9
Rayan25166 <rayanjoomy772@gmail.com>
2024-11-19 22:57:57 +0530

    Add files via upload

---
+++ Day-2/rayan2.py
@@ -0,0 +1,7 @@
+'''AUTHOR=RAYAN JOOMY
+DESCRIPTION=Create a Python Program to Ask for Name and Age
+ Write a Python program that takes the user's name and age as input, then prints a greeting message.'''
+
+name=str(input("enter the name:"))
+age=int(input("enter the age:"))
+print("hello",name,"your age is",age)
