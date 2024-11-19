cf53dbdff48c9e389649b60cf470a0941c197435
Sruthy24 <sruthysmnglm@gmail.com>
2024-11-19 06:53:33 +0530

    Delete Day-2/Sruthy _day 2.py

--- Day-2/Student _day 2.py
+++
@@ -1,6 +0,0 @@
-''' Author -Sruthy S
-Description - Create a Python Program to Ask for Name and Age Objective: Write a Python program that takes the user's name and age as input, then prints a greeting message'''
-name = str(input("Enter your Name:"))
-age = int(input("Enter Your Age:"))
-print(f" Hello,{name}!you are{age}years old")
-   
