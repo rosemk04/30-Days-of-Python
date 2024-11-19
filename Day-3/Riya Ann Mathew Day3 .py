d62175aad444b784ef20bccd0b5a5ebc95b7a50a
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-11-19 09:41:27 -0800

    Add files via upload

---
+++ Day-3/Riya Ann Mathew Day3 .py
@@ -0,0 +1,7 @@
+'''Author:Riya Ann Mathew
+Description:Python program that determines if a person is a minor or an adult based on their age  '''
+age=int(input("Enter your age:"))
+if age <18:
+    print("You are a minor.")
+else:
+     print ("You are a adult.")
