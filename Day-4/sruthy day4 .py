ebd72ba4114ad71e8aec9d0ef471de28b6d798c4
Sruthy24 <sruthysmnglm@gmail.com>
2024-11-20 07:53:53 +0530

    Add files via upload

---
+++ Day-4/sruthy day4 .py
@@ -0,0 +1,8 @@
+'''Author = Sruthy S
+Description= Write a Program to Swap the Values of Two Variables Objective: Create a Python program that exchanges the values of two variables.'''
+a = int(input("enter the x value:"))
+b = int(input("enter the y value:"))
+print("Before swapping:","x=",a,"y=",b)
+print("After swapping:","x=",b,"y=",a)
+
+
