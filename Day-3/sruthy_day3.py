0bed56f229c45da38d15b240061396f7b7e37660
Sruthy24 <sruthysmnglm@gmail.com>
2024-11-20 07:52:50 +0530

    Add files via upload

---
+++ Day-3/sruthy_day3.py
@@ -0,0 +1,9 @@
+'''Author -Sruthy S
+Description -Check Whether a Person is a Minor or an Adult
+ Write a Python program that determines if a person is a minor or an adult based on their age.'''
+age=int(input("Enter your age:"))
+if (age<18):
+    print("you are minor")
+else:
+    print("you are adult")
+   
