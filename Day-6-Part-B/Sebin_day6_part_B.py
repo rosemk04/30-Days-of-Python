c730ea795863d73b3245aad9fce70d2a9f0adc85
SebinPrince <sebinpaikada1@gmail.com>
2024-11-20 22:37:33 +0530

    Create Sebin_day6_part_B.py

---
+++ Day-6-Part-B/Sebin_day6_part_B.py
@@ -0,0 +1,7 @@
+'''AUTHOR=SEBIN PRINCE
+DESCRIPTION= Create a Program to Store and Display a List of Your Favorite Movies Objective: Write a Python program that stores a list of your favorite movies and displays them when requested.
+'''
+favourite_movies=["UNCHARTED","GEOSTROM","IT","GRAN TURISMO"]
+print("My favourites movies are:")
+for movie in favourite_movies:
+    print(movie)
