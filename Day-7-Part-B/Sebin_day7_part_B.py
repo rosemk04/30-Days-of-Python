78a26b5da081785013cbf6f4d94508973991441f
SebinPrince <sebinpaikada1@gmail.com>
2024-11-21 22:18:17 +0530

    Create Sebin_day7_part_B.py

---
+++ Day-7-Part-B/Sebin_day7_part_B.py
@@ -0,0 +1,5 @@
+'''AUTHOR=SEBIN PRINCE
+DESCRIPTION= Ask for the User's Favorite Color and Print a Message Objective: Write a Python program that asks the user for their favorite color and prints a personalized message that includes the color.
+'''
+a=str(input("Enter your favourite color?"))
+print("Wow",a,"is a beautiful color")
