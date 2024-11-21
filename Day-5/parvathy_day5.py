c73785714477f6d6c43ebcd01c1289a8fb66fb45
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-21 21:52:53 +0530

    Add files via upload

---
+++ Day-5/parvathy_day5.py
@@ -0,0 +1,10 @@
+'''
+Author:Parvathy J Nair
+Description: Python program that takes a number as input
+ and determines if it is even or odd
+'''
+number=int(input("Enter a number:"))
+if number%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
