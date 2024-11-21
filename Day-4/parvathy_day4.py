ebeb55e07f4a646c9a071243d5d44dd443cc53f0
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-21 21:38:15 +0530

    Add files via upload

---
+++ Day-4/parvathy_day4.py
@@ -0,0 +1,12 @@
+'''
+Author:Parvathy J Nair
+Description:Program to swap the values of two variables
+'''
+
+x=int(input("Enter the value of x:"))
+y=int(input("Enter the value of y:"))
+print(f"Before swapping:x={x},y={y}")
+var=x
+x=y
+y=var
+print(f"After swapping:x={x},y={y}")
