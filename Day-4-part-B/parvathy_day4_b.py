9bacfb1c4e390f4376d376560a987cd0c214fc5d
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-21 21:47:28 +0530

    Add files via upload

---
+++ Day-4-part-B/parvathy_day4_b.py
@@ -0,0 +1,8 @@
+'''
+Author:Parvathy J Nair
+Description:Python program that calculates and prints the sum of numbers from 1 to 15
+'''
+Total=0
+for number in range(1,16):
+    Total+=number
+print(f"The sum of numbers 1 to 15 is:{Total}")
