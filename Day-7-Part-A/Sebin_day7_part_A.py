05a30cfe561b4116778cad6365b784ccd828db85
SebinPrince <sebinpaikada1@gmail.com>
2024-11-21 22:13:23 +0530

    Create Sebin_day7_part_A.py

---
+++ Day-7-Part-A/Sebin_day7_part_A.py
@@ -0,0 +1,6 @@
+'''AUTHOR=SEBIN PRINCE
+DESCRIPTION=Calculate the Area of a Rectangle Objective: Write a Python program that takes the length and width of a rectangle as inputs and calculates its area.
+'''
+a=float(input("Enter the length:"))
+b=float(input("Enter the breadth:"))
+print("The area of a rectangle is:",a*b)
