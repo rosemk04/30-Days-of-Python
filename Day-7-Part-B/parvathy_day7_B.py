1dac286eb19524830ac37dbe1e6135dfd1a57b5f
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-24 19:09:24 +0530

    Add files via upload

---
+++ Day-7-Part-B/parvathy_day7_B.py
@@ -0,0 +1,8 @@
+'''
+Author:Parvathy J Nair
+Description:Write a Python program that asks the user for their
+favorite color and prints a personalized message that includes the color
+ '''
+
+favourite_color=input("Enter your favourite color:")
+print(f"Wow,{favourite_color} is a beautiful color")
