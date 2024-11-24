3094f51aff053348277fc705270234ba829a89ed
parvathyjnair01 <parvathyj06@gmail.com>
2024-11-24 19:03:38 +0530

    Add files via upload

---
+++ Day-7-Part-A/parvathy_day7_A.py
@@ -0,0 +1,10 @@
+'''
+Author:Parvathy J Nair
+Description:Write a Python program that takes
+the length and width of a rectangle as inputs and calculates its area
+'''
+
+num1=float(input("Enter the length of rectangle:"))
+num2=float(input("Enter width of rectangle:"))
+area=num1*num2
+print(f"The area of rectangle is: {area}")
