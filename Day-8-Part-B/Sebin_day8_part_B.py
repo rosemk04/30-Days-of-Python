7c110eea1053742d46048a4cf0a279ef21e4892e
SebinPrince <sebinpaikada1@gmail.com>
2024-11-26 23:13:43 +0530

    Create Sebin_day8_part_B.py

---
+++ Day-8-Part-B/Sebin_day8_part_B.py
@@ -0,0 +1,6 @@
+'''AUTHOR:SEBIN PRINCE
+DESCRIPTION:Count the Number of Characters in a Word Objective: Write a Python program that takes a word as input and prints the number of characters in it.
+'''
+a=str(input("Enter a word:"))
+b=len(a)
+print("The word",a,"has",b,"characters.")
