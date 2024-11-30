baf13a9167b17e5d483adea6525e290b46414f4b
Sruthy24 <sruthysmnglm@gmail.com>
2024-11-30 06:23:31 +0530

    Add files via upload

---
+++ Day-6-Part-A/sruthy_day6 .py
@@ -0,0 +1,8 @@
+number=int(input("enter a number for multiplication table: "))
+print(f"multiplication table of{ number}:")
+for i in range(1,11):
+    result= number* i
+    print(f"{number}*{i}={result}")
+
+
+
