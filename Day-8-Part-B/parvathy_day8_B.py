45f82b8368de132ebf97f9e47c1c04fff9fd7841
parvathyjnair01 <parvathyj06@gmail.com>
2024-12-04 20:19:56 +0530

    Add files via upload

---
+++ Day-8-Part-B/parvathy_day8_B.py
@@ -0,0 +1,8 @@
+'''
+Author:Parvathy J Nair
+Description:Write a Python program that takes a word as input
+and prints the number of characters in it
+'''
+word=str(input("Enter a word:"))
+character_count=len(word)
+print(f"The word {word} has {character_count} characters ")
