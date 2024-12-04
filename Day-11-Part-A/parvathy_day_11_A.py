33d06c3c6daa73d3c5e538d6238762059fbb030c
parvathyjnair01 <parvathyj06@gmail.com>
2024-12-04 20:45:23 +0530

    Add files via upload

---
+++ Day-11-Part-A/parvathy_day_11_A.py
@@ -0,0 +1,13 @@
+'''
+Author:Parvathy J Nair
+Description: Write a Python program that takes two sets,
+ finds their union and intersection, and displays the results.
+'''
+set1={1,2,3,4,5}
+print("set_1:",set1)
+set2={4,5,6,7,8}
+print("set_2:",set2)
+union=set1|set2
+print("Union:",union)
+intersection=set1&set2
+print("Intersection:",intersection)
